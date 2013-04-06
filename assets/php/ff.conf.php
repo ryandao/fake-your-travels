8214e871068157249110360ca669da7ea2206405
Alex Liew <alex.ljz@gmail.com>
2013-04-06 15:19:18 -0700

    flash foto upload

---
+++ assets/php/ff.conf.php
@@ -0,0 +1,5 @@
+<?php
+
+$conf = array('url' => "http://flashfotoapi.com/api/",
+              'user' => "alexljz",
+              'key' => "jDe8jrGkjzyd1JvN8ZOa7UCpjuscRQ8h");
--- assets/php/ff.php
+++ assets/php/ff.php
@@ -1,13 +1,62 @@
 <?php
 
-include "flashfoto.php";
+require_once "flashfoto.php";
+require_once "ff.conf.php";
 
 
+// for testing only
+$_POST['method'] = 'get';
+$_POST['url'] = 'https://www.filepicker.io/api/file/OGAgCDZESeqNDIAOCc8W';
+$_POST['image_id'] = '178353';
 
-$base_url = "http://flashfotoapi.com/api/";
-$username = "alexljz";
-$apikey = "jDe8jrGkjzyd1JvN8ZOa7UCpjuscRQ8h";
+function dispatch() {
+  $method = $_POST['method'];
 
-$image_url = $_POST['url'];
+  switch ($method) {
+    case "add":
+      ff_add();
+      break;
+    case "get":
+      ff_get();
+      break;
+    case "remove_bg":
+      ff_remove_bg();
+      break;
+    default:
+      break;
+  }
+}
 
-$ff = new FlashFoto($username, $apikey, $base_url);
+function ff_add() {
+  global $conf;
+  $ff = new FlashFoto($conf['user'], $conf['key'], $conf['url']);
+
+  $image_url = $_POST['url'];
+  $response = $ff->add(file_get_contents($image_url));
+
+  echo json_encode($response);
+}
+
+function ff_get() {
+  global $conf;
+  $ff = new FlashFoto($conf['user'], $conf['key'], $conf['url']);
+
+  $image_id = $_POST['image_id'];
+  $response = $ff->get($image_id, array('version' => "UniformBackgroundMasked"));
+
+  // header()
+  echo "<img src='data:image/png;base64," . base64_encode($response) . "'>";
+  // echo base64_encode($response);
+}
+
+function ff_remove_bg() {
+  global $conf;
+  $ff = new FlashFoto($conf['user'], $conf['key'], $conf['url']);
+  
+  $image_id = $_POST['image_id'];
+  $response = $ff->remove_uniform_background($image_id);
+
+  echo json_encode($response);
+}
+
+dispatch();
