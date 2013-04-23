5f2cc41231b2ffc30ab9ab578a4dce105bc73dc7
Ryan Dao <daoduyducduong@gmail.com>
2013-04-22 18:20:08 -0700

    Add load more photo feature

--- assets/js/background_image.js
+++ assets/js/background_image.js
@@ -2,13 +2,17 @@ $(document).ready(function () {
   initStage({});
 
   $('#bg-selector form').submit(function() {
+    debugger
     var modal$ = $('#bg-modal .modal');
-    var resultsDiv$ = $('#bg-modal .modal-body .results')
+    var resultsDiv$ = $('#bg-modal .modal-body .results');
     var loadingMessage$ = $('#bg-modal .message');
+    var loadMoreBtn$ = $('#load-more-btn');
 
     // Empty the modal body and show loading message
     resultsDiv$.empty();
     loadingMessage$.show();
+    // Disable the Load more button while loading
+    loadMoreBtn$.addClass('disabled');
 
     // Query the proxy server
     var searchQuery = $('#bg-selector input').val();
@@ -17,26 +21,63 @@ $(document).ready(function () {
       data: { query: searchQuery },
       type: 'GET',
       success: function(results) {
+        // Hide loading message and enable load more btn
         loadingMessage$.hide();
-        for (var i = 0; i < results.images.length; i++) {
-          var imgHtml = '<a href="#" id="' + i + '"><img src=' + results.images[i].thumbnail.url + ' class="st-bg-image"></a>';
-          resultsDiv$.append(imgHtml);
-        }
+        loadMoreBtn$.removeClass('disabled');
 
-        $('a', resultsDiv$).click(function() {
-          var id = parseInt($(this).attr('id'));
-          window.backgroundImg = results.images[id].url;
-          bg_proxy(backgroundImg);
+        // Display the images
+        addResultImagesToDiv(results.images, resultsDiv$)
 
-          modal$.modal('hide');
-        })
+        // Save the current search query and next search index
+        window.currentSearch = searchQuery;
+        window.startIndex = results.nextPage.startIndex;
       }
     })
 
     modal$.modal();
 
     return false;
-  })
+  });
+
+  $('#load-more-btn').click(function() {
+    debugger
+    var resultsDiv$ = $('#bg-modal .modal-body .results');
+
+    // Show the loading spinner
+    $('#load-more-btn .searching').show();
+
+    $.ajax({
+      url: 'images.php',
+      data: { query: window.currentSearch, start: window.startIndex},
+      type: 'GET',
+      success: function(results) {
+        // Hide the loading spinner
+        $('#load-more-btn .searching').hide();
+
+        // Display the images
+        addResultImagesToDiv(results.images, resultsDiv$)
+
+        // Update the next search index
+        window.startIndex = results.nextPage.startIndex;
+      }
+    })
+  });
+
+  function addResultImagesToDiv(imageArr, resultsDiv) {
+    for (var i = 0; i < imageArr.length; i++) {
+      var imgDOM$ = $('<a href="#" id=' + i + '>').append('<img src=' + imageArr[i].thumbnail.url + ' class="st-bg-image"></a>');
+      resultsDiv.append(imgDOM$);
+
+      $(imgDOM$).click(function() {
+        var id = parseInt($(this).attr('id'));
+        window.backgroundImg = imageArr[id].url;
+        bg_proxy(backgroundImg);
+
+        $('#bg-modal .modal').modal('hide');
+
+      });
+    }
+  }
 
   function bg_proxy(url) {
     jQuery.post("image-proxy.php", {link: url}, update_bg, "json");
--- images.php
+++ images.php
@@ -1,32 +1,37 @@
 <?php
-  include('google_custom_search.php');
 
-  $api_key = 'AIzaSyCSa7KMZCFCit1R3NHW45itRcc_tDv8MNY';
-  $cx = '005628824904312303925:vrivvhjelhi';
+include('google_custom_search.php');
 
-  $api = new GoogleCustomSearch($api_key, $cx);
-  if ($query = $_GET['query']) {
-    $results = $api->searchImages($query . ' scenery');
+$api_key = 'AIzaSyCSa7KMZCFCit1R3NHW45itRcc_tDv8MNY';
+$cx = '005628824904312303925:vrivvhjelhi';
 
-    $images = array('images' => array());
-    for ($i = 0; $i < count($results->items); $i++) {
-      $image = array(
-        'url' => $results->items[$i]->link,
-        'height' => $results->items[$i]->image->height,
-        'width' => $results->items[$i]->image->width,
-        'thumbnail' => array(
-          'url' => $results->items[$i]->image->thumbnailLink,
-          'height' => $results->items[$i]->image->thumbnailHeight,
-          'width' => $results->items[$i]->image->thumbnailWidth
-        ),
-        'nextPage' => array(
-          'startIndex' => $results->queries->nextPage[0]->startIndex
-        )
-      );
-      array_push($images['images'], $image);
-    }
-
-    header('Content-Type: application/json');
-    echo json_encode($images);
+$api = new GoogleCustomSearch($api_key, $cx);
+if ($query = $_GET['query']) {
+  $start = 1;
+  if (isset($_GET['start'])) {
+    $start = $_GET['start'];
   }
-?>
+  $results = $api->searchImages($query . ' scenery', null, $start);
+
+  $images = array('images' => array());
+  for ($i = 0; $i < count($results->items); $i++) {
+    $image = array(
+      'url' => $results->items[$i]->link,
+      'height' => $results->items[$i]->image->height,
+      'width' => $results->items[$i]->image->width,
+      'thumbnail' => array(
+        'url' => $results->items[$i]->image->thumbnailLink,
+        'height' => $results->items[$i]->image->thumbnailHeight,
+        'width' => $results->items[$i]->image->thumbnailWidth
+      ),
+      'nextPage' => array(
+        'startIndex' => $results->queries->nextPage[0]->startIndex
+      )
+    );
+    array_push($images['images'], $image);
+  }
+  $images['nextPage'] = array('startIndex' => $results->queries->nextPage[0]->startIndex);
+
+  header('Content-Type: application/json');
+  echo json_encode($images);
+}
